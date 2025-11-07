6f2e0b4edca83a582b7770a32050ac66cd5535b9
SanTeseu <matheussant1805@gmail.com>
2025-11-06 23:30:36 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Minha Biblioteca Pessoal</h1>
+    <p>Total de livros: {{ livros.length }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const livros = ref([])
+</script>
